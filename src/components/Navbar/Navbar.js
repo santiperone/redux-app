445be38cdc97a13931ccd6638a7cb74247c58159
santiperone <sdperone97@gmail.com>
2021-01-17 18:03:59 -0300

    navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import './Navbar.css';
+
+function Botonera() {
+  return (
+    <div className="Navbar">
+        <header className="App-header">
+            <div>
+                <h1>Título</h1>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>List</li>
+                <li>New</li>
+            </ul>
+        </header>
+    </div> 
+  );
+}
+
+export default Botonera;
