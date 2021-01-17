d5d0debfe7fc22bc97f9d0c9c72a2e082d65acf4
santiperone <sdperone97@gmail.com>
2021-01-17 18:30:50 -0300

    navbar item click changes title

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,39 @@
 import './Navbar.css';
 
-function Botonera() {
+import navState from '../../redux/reducers/navReducer';
+import {nav_click} from '../../redux/actions/navActions';
+
+import {connect} from 'react-redux';
+
+function Botonera({title, nav_click}) {
+  const buttonClick = (title) => {
+    nav_click({
+      title: title,
+    });
+  }
+
   return (
     <div className="Navbar">
         <header className="App-header">
             <div>
-                <h1>Título</h1>
+                <h1>{title}</h1>
             </div>
             <ul>
-                <li>Home</li>
-                <li>List</li>
-                <li>New</li>
+                <li onClick={() => buttonClick('Home')}>Home</li>
+                <li onClick={() => buttonClick('List')}>List</li>
+                <li onClick={() => buttonClick('New')}>New</li>
             </ul>
         </header>
     </div> 
   );
 }
 
-export default Botonera;
+const mapStateToProps = (state) => {
+  return {
+    title: state.navState.title,
+  }
+}
+export default connect(
+  mapStateToProps,
+  { nav_click }
+)(Botonera);
